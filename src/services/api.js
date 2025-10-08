56b1c4f5779499d2b0d2f104eae12f10bd15744c
Shaxboz <89291663+Shaxboz2001@users.noreply.github.com>
2025-10-08 22:41:45 +0500

    Update api.js /

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://lms-backend-grdt.onrender.com/"; // Sizning FastAPI server manzilingiz
+const BASE_URL = "https://lms-backend-grdt.onrender.com"; // Sizning FastAPI server manzilingiz
 const token = localStorage.getItem("token"); // JWT tokenni localStorage'dan o'qiymiz
 
 const config = {
